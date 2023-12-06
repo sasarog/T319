fedc3519214e9dc91314527845c0fb75051ea876
Aleksandr Rogalevich <sasa13481@gmail.com>
2023-12-06 18:23:26 +0300

    Написали Main

---
+++ Hash/HashMain.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "HasTable.h"
+using namespace std;
+
+int main() {
+	HasTable eva(15);
+	eva.add("Eva", 20);
+	eva.add("Adam", 32);
+	eva.add("Kain", 15);
+	eva.add("Avel", 12);
+	eva.add("Сиф", 5);
+	eva.print();
+
+}
