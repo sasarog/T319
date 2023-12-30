c2985f05e4296818939b15e9b76a2b6ba6ba941e
Aleksandr Rogalevich <sasa13481@gmail.com>
2023-12-30 13:50:57 +0300

    Написали виртуальный метод.

--- Classes/ClassesMain.cpp
+++ Classes/ClassesMain.cpp
@@ -1,7 +1,15 @@
 #include "Student.h"
 using namespace std;
+void f(Chelovek* c) {
+	c->print();
+}
+void f(Student* c) {
+	c->print();
+}
+
 
 int main() {
 	Student eva("EVA", 20, 8);
-	eva.print();
+	Chelovek adam("ADAM", 30);
+	f(&eva);
 }
